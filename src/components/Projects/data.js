dc5af1601aeef8e0279e20446e1b0c7a2e94d51f
haraphat01 <haraphat@gmail.com>
2023-06-08 07:50:14 -0700

    Updated my portfoli

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,17 +1,34 @@
 
 import redus from "../../images/redus.png"
-import shooter from "../../images/shooter.png"
+import pencil from "../../images/pencilChat.png"
+import pic from "../../images/pic.png"
+import biz from "../../images/bizz.png"
 
 export const personal = [
+
   {
-     src: redus,
-     desc: "This is an app made with redux-toolkit for state's management",
-     link: "https://benevolent-tarsier-e189cd.netlify.app"
+    src: pencil,
+    desc: "A telegram bot that uses LLM to enhance the chat experience for users, with close to 3k users. ",
+    link: "https://t.me/pencilAI",
   },
+
   {
-    src: shooter,
-    desc: "This game has 1 object - Kill as much virus as possible.",
-    link: "https://raw.githack.com/haraphat01/javascript/feature/index.html",
+    src: redus,
+    desc: "This is an app made with redux-toolkit for state's management",
+    link: "https://benevolent-tarsier-e189cd.netlify.app"
+  },
+
+  {
+    src: pic,
+    desc: "Pencilpix is a Telegram bot that allows users to generate custom images using artificial intelligence through prompts.",
+    link: "https://github.com/haraphat01/imagebot",
+  },
+
+  {
+    src: biz,
+    desc: "A platform to connect business buyer and seller, still in development",
+    link: "https://bizzconnect.vercel.app/",
   }
+
 
 ]
